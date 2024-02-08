18d2ff1b353041e6c1efdab67884ef233154ec7f
Koichi Nakata <126118326+Claudioflew@users.noreply.github.com>
2024-02-07 20:07:09 -0800

    Added insertion sort

--- src/main/java/org/example/Assignment3.java
+++ src/main/java/org/example/Assignment3.java
@@ -4,6 +4,8 @@ package org.example;
 
 import java.util.Scanner;
 
+import static java.util.Collections.min;
+
 public class Assignment3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -30,31 +32,51 @@ public class Assignment3 {
     }
 
     public static int[] merge(int[] list1, int[] list2) {
-        int[] merged = new int[list1.length + list2.length];
+        int size1 = list1.length, size2 = list2.length;
+        int[] merged = new int[size1 + size2];
 
-        // We want to iterate two arrays at the same time
-        int index1 = 0, index2 = 0;
-        while (index1 < list1.length || index2 < list2.length) {
-            // If we finished iterating list1
-            if (index1 >= list1.length) {
-                merged[index1 + index2] = list2[index2];
-                index2++;
-                // Else if we finished iterating list2
-            } else if (index2 >= list2.length) {
-                merged[index1 + index2] = list1[index1];
-                index1++;
-                // Else both lists can be still iterated
-            } else {
-                if (list1[index1] < list2[index2]) {
-                    merged[index1 + index2] = list1[index1];
-                    index1++;
-                } else {
-                    merged[index1 + index2] = list2[index2];
-                    index2++;
-                }
-            }
+        for (int i = 0; i < size1+size2; i++) {
+            if (i >= size1) merged[i] = list2[i-size1];
+            else merged[i] = list1[i];
         }
+
+        insertionSort(merged, size1);
+
+//        // We want to iterate two arrays at the same time
+//        int index1 = 0, index2 = 0;
+//        while (index1 < list1.length || index2 < list2.length) {
+//            // If we finished iterating list1
+//            if (index1 >= list1.length) {
+//                merged[index1 + index2] = list2[index2];
+//                index2++;
+//                // Else if we finished iterating list2
+//            } else if (index2 >= list2.length) {
+//                merged[index1 + index2] = list1[index1];
+//                index1++;
+//                // Else both lists can be still iterated
+//            } else {
+//                if (list1[index1] < list2[index2]) {
+//                    merged[index1 + index2] = list1[index1];
+//                    index1++;
+//                } else {
+//                    merged[index1 + index2] = list2[index2];
+//                    index2++;
+//                }
+//            }
+//        }
         // Return by reference
         return merged;
+    }
+
+    private static void insertionSort(int[] array, int start) {
+        for (int i = start; i < array.length; i++) {
+            int j = i;
+            while (j > 0 && array[j] < array[j-1]) {
+                int temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+                j--;
+            }
+        }
     }
 }
